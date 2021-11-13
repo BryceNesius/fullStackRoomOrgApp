e326d8fc157fa00479c5a5106267e9ab2b6a127e
Kayla Rehwoldt <kayla_rehwoldt@taylor.edu>
2021-11-12 21:34:44 -0500

    Added dorm and school relationMappings

---
+++ api/models/school.js
@@ -0,0 +1,31 @@
+const {Model} = require("objection");
+const {dorm} = require('./dorm.js');
+const {user} = require('./user.js');
+
+class school extends Model {
+    static get tableName() {
+        return 'school';
+    }
+    static get relationMappings() {
+        return {
+            dorms: {
+                relation: Model.HasManyRelation,
+                modelClass: dorm,
+                join: {
+                    from: 'school_id',
+                    to: 'dorm.school_id'
+                }
+            }, // end dorms
+            school_users: {
+                relation: Model.HasManyRelation,
+                modelClass: user,
+                join: {
+                    from: 'school_id',
+                    to: 'user.school_id'
+                }
+            } // end school_users
+        } // end return
+    } // end relationMappings
+} // end class school
+
+module.exports = { school };
