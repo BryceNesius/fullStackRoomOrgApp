e047edd1522878d309cf51b8e4e68bb21f220d4e
Bryce Nesius <bnesius@Bryces-MacBook-Pro.local>
2021-12-09 23:55:23 -0500

    created Create a new disign page, still needs functionality finished to actually post to the database

---
+++ src/pages/CreateDesign.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container>
+
+  <h4>Create a new design</h4>
+    <v-form v-model="valid">
+      <v-text-field
+        label="Plan Name"
+        v-model="createDesign.name"
+        required
+      >
+      </v-text-field>
+
+      <v-text-field
+          label="Plan Description"
+          v-model="createDesign.description"
+      >
+      </v-text-field>
+
+      <v-text-field
+          label="What school is this plan for?"
+          v-model="createDesign.school"
+          required
+      >
+      </v-text-field>
+      <v-text-field
+          label="What dorm is this plan for?"
+          v-model="createDesign.dorm"
+          required
+      >
+      </v-text-field>
+
+      <v-btn v-bind:disabled="!valid" v-on:click="handleSubmit"
+      >Create Plan
+      </v-btn>
+
+    </v-form>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CreateDesign",
+
+  data: function () {
+    return {
+      valid: false,
+
+      createDesign: {
+        name: "",
+        description: "",
+        school: "",
+        dorm: ""
+
+
+      },
+      rules: {
+        required: [(val) => val.length > 0 || "Required"],
+        name: [(val) => /[a-z]/.test(val) || "Need lower case letter"],
+        school: [(val) => /[a-z]/.test(val) || "Need lower case letter"],
+        dorm: [(val) => /[a-z]/.test(val) || "Need lower case letter"]
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
