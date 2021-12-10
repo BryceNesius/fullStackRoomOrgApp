092a960e04baa385498416387d2f9152fc6dbb7d
Bryce Nesius <bnesius@Bryces-MacBook-Pro.local>
2021-12-09 23:10:39 -0500

    ade things look better on Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,8 +5,8 @@
 
       <p class="body-1">Acorns is a service that allows you to design and upload room designs to plan for college or just to share with friends!</p>
     </div>
-    <img src="../media/room1.jpeg" style="height: 500px; width: 500px" alt="Taylor University Example Room">
-    <img src="../media/room2.jpeg" style="height: 500px; width: 500px" alt="Taylor University Example Room">
+    <img src="../media/room1.jpeg" style="height: 500px; width: 480px; display: inline; margin-right: 50px" alt="Taylor University Example Room">
+    <img src="../media/room2.jpeg" style="height: 504px; width: 602px; display: inline" alt="Taylor University Example Room">
 
   </v-container>
 </template>
