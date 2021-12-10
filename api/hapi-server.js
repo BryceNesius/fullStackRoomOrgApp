1e356e1f0ee947613414cc2a11ab34b83ca26f8a
Kayla Rehwoldt <kayla_rehwoldt@taylor.edu>
2021-12-09 21:20:37 -0500

    here it is

--- api/hapi-server.js
+++ api/hapi-server.js
@@ -1,73 +1,215 @@
-// Change to acorns database
+// Knex
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "pg.cse.taylor.edu",
-    user: "acorn",
-    password: "qijihiyo",
-    database: "acorn",
+    host: "pg.cse.taylor.edu", // PostgreSQL server
+    user: "kayla_rehwoldt", // Your user name
+    password: "zatuyama", // Your password
+    database: "kayla_rehwoldt", // Your database name
   },
 });
 
-// Configure Objection.
-const { Model } = require("objection");
-Model.knex(knex);
+// Objection
+const objection = require("objection");
+objection.Model.knex(knex);
 
-// Load model classes.
-const DesignPlans = require("./models/design_plans");
-const Dorm = require("./models/dorm");
-const Favorites = require("./models/favorites");
-const Furniture = require("./models/furniture");
-const Ownership = require("./models/ownership");
-const Room = require("./models/room");
-const RoomFurniture = require("./models/furniture");
-const School = require("./models/school");
-const User = require("./models/user");
-const UserFriends = require("./models/user_friends");
+// Models
+const Account = require("./models/Account");
 
-// Configure Hapi.
-const Hapi = require("@hapi/hapi");
-const Boom = require("@hapi/boom");
-const Joi = require("joi");
+// Hapi
+const Joi = require("@hapi/joi"); // Input validation
+const Hapi = require("@hapi/hapi"); // Server
 
-const init = async () => {
-  const server = Hapi.server({
-    host: "localhost",
-    port: 3000,
-  });
+const server = Hapi.server({
+  host: "localhost",
+  port: 3000,
+  routes: {
+    cors: true,
+  },
+});
 
-  // Show routes.
+async function init() {
+  // Show routes at startup.
   await server.register(require("blipp"));
 
-  // Log stuff.
+  // Output logging information.
   await server.register({
     plugin: require("hapi-pino"),
     options: {
-      transport: {
-        target: "pino-pretty"
-      }
+      prettyPrint: true,
     },
   });
 
+  // Configure routes.
   server.route([
     {
-      method: "GET",
-      path: "/",
-      handler: (request, h) => "Hello, Hapi",
+      method: "POST",
+      path: "/accounts",
+      config: {
+        description: "Sign up for an account",
+        validate: {
+          payload: Joi.object({
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+          }),
+        },
+      },
+      handler: async (request, h) => {
+        const existingAccount = await Account.query()
+          .where("email", request.payload.email)
+          .first();
+        if (existingAccount) {
+          return {
+            ok: false,
+            msge: `Account with email '${request.payload.email}' is already in use`,
+          };
+        }
+
+        const newAccount = await Account.query().insert({
+          first_name: request.payload.firstName,
+          last_name: request.payload.lastName,
+          email: request.payload.email,
+          password: request.payload.password,
+        });
+
+        if (newAccount) {
+          return {
+            ok: true,
+            msge: `Created account '${request.payload.email}'`,
+          };
+        } else {
+          return {
+            ok: false,
+            msge: `Couldn't create account with email '${request.payload.email}'`,
+          };
+        }
+      },
     },
 
     {
       method: "GET",
-      path: "/users",
+      path: "/accounts",
+      config: {
+        description: "Retrieve all accounts",
+      },
       handler: (request, h) => {
-        return User.query();
+        return Account.query();
+      },
+    },
+
+    {
+      method: "DELETE",
+      path: "/accounts/{id}",
+      config: {
+        description: "Delete an account",
+      },
+      handler: (request, h) => {
+        return Account.query()
+          .deleteById(request.params.id)
+          .then((rowsDeleted) => {
+            if (rowsDeleted === 1) {
+              return {
+                ok: true,
+                msge: `Deleted account with ID '${request.params.id}'`,
+              };
+            } else {
+              return {
+                ok: false,
+                msge: `Couldn't delete account with ID '${request.params.id}'`,
+              };
+            }
+          });
+      },
+    },
+    {
+      method: "PATCH",
+      path: "/reset-password",
+      config: {
+        description: "Reset Password",
+        validate: {
+          payload: Joi.object({
+            email: Joi.string().email().required(),
+            oldPassword: Joi.string().min(8).required(),
+            newPassword: Joi.string().min(8).required(),
+            confirmNewPassword: Joi.string().min(8).required(),
+          }),
+        },
+      },
+      handler: async (request, h) => {
+        const account = await Account.query()
+            .where("email", request.payload.email)
+            .first();
+        if (
+            account &&
+            (await account.verifyPassword(request.payload.oldPassword))
+        ) {
+            if( request.payload.newPassword === request.payload.confirmNewPassword) {
+              await account.query().patchAndFetch({
+                password: request.payload.newPassword
+              });
+
+              return {
+                ok: true,
+                msge: `Reset password successfully, user '${request.payload.email}'`,
+              }
+            }
+        } else {
+          return {
+            ok: false,
+            msge: "Invalid email or password",
+
+          };
+        }
+
+
       }
     },
 
+    {
+      method: "POST",
+      path: "/login",
+      config: {
+        description: "Log in",
+        validate: {
+          payload: Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.string().min(8).required(),
+          }),
+        },
+      },
+      handler: async (request, h) => {
+        const account = await Account.query()
+          .where("email", request.payload.email)
+          .first();
+        if (
+          account &&
+          (await account.verifyPassword(request.payload.password))
+        ) {
+          return {
+            ok: true,
+            msge: `Logged in successfully as '${request.payload.email}'`,
+            details: {
+              id: account.id,
+              firstName: account.first_name,
+              lastName: account.last_name,
+              email: account.email,
+            },
+          };
+        } else {
+          return {
+            ok: false,
+            msge: "Invalid email or password",
+          };
+        }
+      },
+    },
   ]);
 
-  console.log("Server listening on", server.info.uri);
+  // Start the server.
   await server.start();
-};
+}
 
+// Go!
 init();
